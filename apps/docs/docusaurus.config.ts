1c1836a7737b915b71c1850d6f84afb8f024d030
Geoffroy <geoffroy.lindas@gmail.com>
2025-04-28 18:26:21 +0000

    fix: remove things

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -59,8 +59,6 @@ const config: Config = {
   },
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Stack Starter',
       logo: {
