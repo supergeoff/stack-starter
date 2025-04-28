90e5b84af1b8796cef74dc9caef054dc6abc6c42
Geoffroy <geoffroy.lindas@gmail.com>
2025-04-28 22:35:10 +0200

    Update docusaurus.config.ts

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
           items: [
             {
               label: 'Documentation',
-              to: '/docs/intro',
+              to: '/docs/techstack',
             },
           ],
         },
