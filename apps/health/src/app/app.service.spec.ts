604ca8f5c9858dfad88e3bc516cfd2dc0b4551c2
Geoff <geoffroy.lindas@gmail.com>
2025-05-01 12:21:52 +0000

    fix: switch to webpack:webpack to allow hot reload in serve (nx known bug)

--- apps/health/src/app/app.service.spec.ts
+++ apps/health/src/app/app.service.spec.ts
@@ -13,8 +13,8 @@ describe('AppService', () => {
   });
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
-      expect(service.getData()).toEqual({ message: 'Hello API' });
+    it('should return "Check"', () => {
+      expect(service.getData()).toEqual({ message: 'Check' });
     });
   });
 });
