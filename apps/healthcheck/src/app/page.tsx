653984742bf2447555abecd13451ab291536cbdb
Geoffroy <geoffroy.lindas@gmail.com>
2025-05-08 16:00:07 +0100

    Revert-to-update (#24)
    * feat: add next app
    
    * fix: lint error due to import type
    
    * chore: migrate nx to latest

---
+++ apps/healthcheck/src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css';
+
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return <div className={styles.page}></div>;
+}
