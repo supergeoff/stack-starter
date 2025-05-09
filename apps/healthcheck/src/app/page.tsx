ddc49fd970a17f783bc3c333d343b8bfb9f3fc44
Geoff <geoffroy.lindas@gmail.com>
2025-05-09 09:26:42 +0000

    feat: setup tailwind & fix lint rules

--- apps/healthcheck/src/app/page.tsx
+++ apps/healthcheck/src/app/page.tsx
@@ -1,10 +1,16 @@
 import styles from './page.module.css';
 
-export default function Index() {
+export default function Page() {
   /*
    * Replace the elements below with your own.
    *
    * Note: The corresponding styles are in the ./index.css file.
    */
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <div className='text-2xl text-pink-200 font-bold bg-pink-500 p-2 font-mono'>
+        Hello
+      </div>
+    </div>
+  );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: ['**/dist', '**/.next', '**/.docusaurus'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
