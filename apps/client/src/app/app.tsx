a09905d1430e68ab37f6581bcb9300e76206c353
Geoff <geoffroy.lindas@gmail.com>
2025-05-16 11:12:17 +0000

    feat: add shadcn in react lib

---
+++ apps/client/src/app/app.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@stack-starter/ui/components/button';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span className='text-3xl font-bold underline'> Hello there, </span>
+        Welcome client 👋
+        <Button>Hello</Button>
+      </h1>
+    </div>
+  );
+}
+export default App;
